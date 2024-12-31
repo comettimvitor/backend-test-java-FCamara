1d43fdeaf1acad48f39c759c18dbe30a6e8cf4ff
unknown <comettivitor@gmail.com>
2024-12-30 22:10:28 -0300

    First commit

---
+++ src/test/java/com/backend_test_java/FCamara/FCamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_test_java.FCamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FCamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
